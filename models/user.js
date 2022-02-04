9cd7b0c7ef8607b4146c1dc28c97a73f83de8c77
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-04 18:10:32 -0300

    criando migrate para criar usuario

---
+++ models/user.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+ 
+  return User;
+};
