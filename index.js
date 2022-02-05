424be0bc3583c5f82ee0499d741f7b5dc2563e9d
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-05 17:51:47 -0300

    Requisito 7

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const UserController = require('./controllers/UserController');
 const LoginController = require('./controllers/LoginController');
 const CategoriesController = require('./controllers/CategoriesController');
+const BlogPostsController = require('./controllers/BlogPostsController');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 app.use('/user', UserController);
 app.use('/login', LoginController);
 app.use('/categories', CategoriesController);
+app.use('/post', BlogPostsController);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
