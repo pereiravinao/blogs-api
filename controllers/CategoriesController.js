3b14523d2f7bb84470cffb3342eef0b57924551a
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-05 16:43:29 -0300

    Criando controller para Categories

---
+++ controllers/CategoriesController.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const CategoriesService = require('../services/CategoriesService');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const categoryCreated = await CategoriesService.create(req.body);
+  if (categoryCreated.code) { 
+    return res.status(categoryCreated.code).json({ message: categoryCreated.message }); 
+}
+  res.status(201).json(categoryCreated);
+});
+
+  module.exports = router;
