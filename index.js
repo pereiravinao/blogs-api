1da6f8704e8994761b561a40472f90f7e2635580
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-05 14:38:51 -0300

    Req 3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const UserController = require('./controllers/UserController');
 const LoginController = require('./controllers/LoginController');
