9adc9bc4f64146f40a8eb1f8595f894d7f6c4675
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-05 13:41:10 -0300

    Req 2

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const UserController = require('./controllers/UserController');
+const LoginController = require('./controllers/LoginController');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/user', UserController);
+app.use('/login', LoginController);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
