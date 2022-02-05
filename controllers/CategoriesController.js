45eb966d1346618e977d7bb7dac9655baeec2dee
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-05 17:23:15 -0300

    Camada Controller para listar todas categorias

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -11,4 +11,12 @@ router.post('/', async (req, res) => {
   res.status(201).json(categoryCreated);
 });
 
+router.get('/', async (req, res) => {
+  const categoryCreated = await CategoriesService.findAll(req);
+  if (categoryCreated.code) { 
+    return res.status(categoryCreated.code).json({ message: categoryCreated.message }); 
+}
+  res.status(200).json(categoryCreated);
+});
+
   module.exports = router;
