a1d548d76c812bca7e6c5b8716d594c71ed80d0c
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-05 16:49:23 -0300

    Refatorando

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -4,7 +4,7 @@ const CategoriesService = require('../services/CategoriesService');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  const categoryCreated = await CategoriesService.create(req.body);
+  const categoryCreated = await CategoriesService.create(req);
   if (categoryCreated.code) { 
     return res.status(categoryCreated.code).json({ message: categoryCreated.message }); 
 }
