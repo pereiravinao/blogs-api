cf67cc8d3f28a64277789da2d6fbadb99e398d23
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-07 18:54:37 -0300

    Requisito 9

--- controllers/BlogPostsController.js
+++ controllers/BlogPostsController.js
@@ -11,6 +11,14 @@ router.post('/', async (req, res) => {
   res.status(201).json(postCreated);
 });
 
+router.get('/:id', async (req, res) => {
+  const postCreated = await BlogPostsService.getById(req);
+  if (postCreated.code) { 
+    return res.status(postCreated.code).json({ message: postCreated.message }); 
+}
+  res.status(200).json(postCreated);
+});
+
 router.get('/', async (req, res) => {
   const postCreated = await BlogPostsService.listAll(req);
   if (postCreated.code) { 
