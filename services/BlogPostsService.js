9404e096d87f5be26244b3f535f4d74d5a16a513
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-07 13:44:46 -0300

    Criando o post na tabela com validacao de conteudo apenas

--- services/BlogPostsService.js
+++ services/BlogPostsService.js
@@ -1,11 +1,26 @@
 const { BlogPosts } = require('../models');
 const auth = require('../middlewares/auth');
+const { schemaPost } = require('../middlewares/validations');
 
 const create = async ({ body, headers }) => {
   const authorized = auth.authentication(headers.authorization);
   if (authorized.code) { return authorized; }
 
-  BlogPosts.create(body);
+  const response = schemaPost.validate(body);
+  if (response.error) { return { code: 400, message: response.error.details[0].message }; }
+
+    // const findCategorie = body.categoryIds.forEach((id) => {
+    //   const categorie = Categories.findOne({ where: { id } });
+    //   return categorie;
+    // });
+    // console.log(findCategorie);
+
+  BlogPosts.create({ 
+    title: body.title, 
+    content: body.content,
+    published: Date.now(),
+    updated: Date.now(), 
+  });
 
   return body;
 };
